642fbcdbe36e82133b1d7df832185733ec6df96a
twattley <wattley83@gmail.com>
2024-10-25 13:59:57 +0100

    remove simulator

--- src/helpers/logging_config.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(
-    format="%(asctime)s - %(levelname)-8s [%(filename)s:%(lineno)s] - %(message)s",
-    level=logging.INFO,
-    datefmt="%Y-%m-%dT%H:%M:%SZ",
-)
-
-logger = logging.getLogger(__name__)
